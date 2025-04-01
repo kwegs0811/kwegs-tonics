64d2de944b7ad30e973f91efd964cb87399c5a67
KWEGS TONICS <francisdavid0811@gmail.com>
2025-04-01 21:26:13 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -43,3 +43,11 @@ function checkout() {
     
     window.location.href = waLink;
 }
+function toggleMenu() {
+    let menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
